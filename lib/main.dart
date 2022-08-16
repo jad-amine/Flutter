a46aa3e7fd13c353c9630f7cab5d49d355815cdf
jad amine <jade1992amine@gmail.com>
2022-08-16 08:43:17 +0300

    Added MaterialApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Hello World'),
+  home: Scaffold(     // home is a property of MaterialApp (properties can be widgets or styles or ... )
+    appBar: AppBar(
+      title: Text('my first app'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text("hello ninja"),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: ()=>{},
+      child: Text("hi"),
+    ),
+  ),
 ));
