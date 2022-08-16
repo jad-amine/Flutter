4529e55e4fc26d52b921751422ad17de7d46c65f
jad amine <jade1992amine@gmail.com>
2022-08-16 14:05:28 +0300

    Added styling

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,25 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(     // home is a property of MaterialApp (properties can be widgets or styles or ... )
     appBar: AppBar(
-      title: Text('my first app'),
+      title: const Text('my first app'),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
-    body: Center(
-      child: Text("hello ninja"),
+    body: const Center(
+      child: Text(
+        "hello ninja",
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+          color: Colors.grey,
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
-      onPressed: ()=>{},
-      child: Text("hi"),
+      onPressed: (){},
+      backgroundColor: Colors.red[600],
+      child: const Text("hi"),
     ),
   ),
 ));
