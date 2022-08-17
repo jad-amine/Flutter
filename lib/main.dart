6b7596e6e9e401007598031d31a9f62bc14b3d8b
jad amine <jade1992amine@gmail.com>
2022-08-17 19:40:11 +0300

    Added Column Widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,33 @@ class UserCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: const Text("User ID Card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          children: const [
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2
+              ),
+            ),
+            Text(
+              'Chun-Li',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
